5bc7276cdf4777e6554681b9a56a391ed8ff5eee
Sebastian Holzgreve <sebastian.holzgreve@web.de>
2022-05-23 23:00:32 +0200

    Added another test for TextVariable

--- tests/test_text_variable.py
+++ tests/test_text_variable.py
@@ -150,3 +150,80 @@ def test_textvariable_sends_config_data_correct():
     assert request == awaited_request_bytes
 
     
+
+def test_textvariable_settings_to_json_contains_settings_set_text_variable():
+    textVar = TextVariable(None, 0x1000, 0x4242, 10)
+
+    textVar.data_address = 1
+    textVar.x_pos = 2
+    textVar.y_pos = 3
+    textVar.color = 4
+    textVar.upperleft_x = 5
+    textVar.upperleft_y = 6
+    textVar.lowerright_x = 7
+    textVar.lowerright_y = 8
+    textVar.text_length = 9
+    textVar.font0_id = 10
+    textVar.font1_id = 11
+    textVar.font_height = 12
+    textVar.font_width = 13
+    textVar.encode_mode = 14
+    textVar.hor_dis = 15
+    textVar.ver_dis = 16
+
+    settings_json = textVar.settings_to_json()
+
+    x_obj = settings_json.get("x")
+    assert int(x_obj) == textVar.x_pos
+
+    y_obj = settings_json.get("y")
+    assert int(y_obj) == textVar.y_pos
+
+    color_obj = settings_json.get("color")
+    assert int(color_obj) == textVar.color
+
+    upperleft_x_obj = settings_json.get("upper_left_x")
+    assert int(upperleft_x_obj) == textVar.upperleft_x
+
+    upperleft_y_obj = settings_json.get("upper_left_y")
+    assert int(upperleft_y_obj) == textVar.upperleft_y
+
+    lower_right_x_obj = settings_json.get("lower_right_x")
+    assert int(lower_right_x_obj) == textVar.lowerright_x
+    
+    lower_right_y_obj = settings_json.get("lower_right_y")
+    assert int(lower_right_y_obj) == textVar.lowerright_y
+
+    text_length_obj = settings_json.get("text_length")
+    assert int(text_length_obj) == textVar.text_length
+
+    font0_id_obj = settings_json.get("font0_id")
+    assert int(font0_id_obj) == textVar.font0_id
+
+    font1_id_obj = settings_json.get("font1_id")
+    assert int(font1_id_obj) == textVar.font1_id
+
+    font_width_obj = settings_json.get("font_width")
+    assert int(font_width_obj) == textVar.font_width
+
+    font_height_obj = settings_json.get("font_height")
+    assert int(font_height_obj) == textVar.font_height
+
+    encode_mode_obj = settings_json.get("encode_mode")
+    assert int(encode_mode_obj) == textVar.encode_mode
+
+    hor_dis_obj = settings_json.get("horizontal_distance")
+    assert int(hor_dis_obj) == textVar.hor_dis
+
+    ver_dis_obj = settings_json.get("vertical_distance")
+    assert int(ver_dis_obj) == textVar.ver_dis
+
+
+
+    
+
+
+
+
+
+
