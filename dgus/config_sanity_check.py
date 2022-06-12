2f51d346223a2d2cd48be7dbef33001178202870
Sebastian Holzgreve <sebastian.holzgreve@web.de>
2022-06-12 20:47:38 +0200

    Reworked Config Sanity Checker
    Reworked the sanity checker, that all pylint problems are gone.

--- dgus/config_sanity_check.py
+++ dgus/config_sanity_check.py
@@ -26,17 +26,17 @@ from dgus.display.display import Display
 def deserialize_json_check(disp : Display) -> bool:
     display_json_file = os.path.join(os.getcwd(), "config", "display.json")
 
-    json_ok = False
-    with open(display_json_file) as json_file:
+    display_json_deserialized = False
+    with open(display_json_file, encoding="utf-8") as json_file:
         json_data = json.load(json_file)
-        json_ok = disp.from_json(json_data)
-        if json_ok is True:
+        display_json_deserialized = disp.from_json(json_data)
+        if display_json_deserialized is True:
             print("JSON Config: OK")
 
-    return json_ok
+    return display_json_deserialized
 
 def generate_data_area_dict_and_print_masks_and_ctrl(disp : Display) -> defaultdict(list):
-    data_area_dict = defaultdict(list)
+    generated_data_area_dict = defaultdict(list)
     print("Listing of all Display Masks and Controls:")
     for mask in disp.displayMasks:
         print(f'Mask {mask.mask_no:02}')
@@ -50,60 +50,62 @@ def generate_data_area_dict_and_print_masks_and_ctrl(disp : Display) -> defaultd
 
             i = ctrl.dataAddress
             while i < ctrl.dataAddress + ctrl.dataLength:
-                data_area_dict[i].append(f'Mask[{mask.mask_no}] Ctrl[{ctrl_idx}] DataAddress')
+                generated_data_area_dict[i].append(f'Mask[{mask.mask_no}] Ctrl[{ctrl_idx}] DataAddress')
                 i += 1
 
             i = ctrl.configAddress
             while i <= ctrl.configAddress + ctrl.configLength:
-                data_area_dict[i].append(f'Mask[{mask.mask_no}] Ctrl[{ctrl_idx}] ConfigAddress')
+                generated_data_area_dict[i].append(f'Mask[{mask.mask_no}] Ctrl[{ctrl_idx}] ConfigAddress')
                 i += 1
 
             ctrl_idx += 1
 
-    return data_area_dict
+    return generated_data_area_dict
 
-def print_overlapping_memory_areas(data_area_dict : defaultdict(list)) -> bool:
+def print_overlapping_memory_areas(data_area_dictonary : defaultdict(list)) -> bool:
     print("Overlapping Memory Areas:")
     address = "Address"
     controls = "Controls"
-    found_overlapping_addresses = False
+    found_an_overlapping_addresses = False
     print(f"{address:<10}{controls:<12}")
-    for key, value in data_area_dict.items():
+    for key, value in data_area_dictonary.items():
         if len(value) > 1:
-            found_overlapping_addresses = True
+            found_an_overlapping_addresses = True
             print(f'{hex(key):<10}{value}')
 
-    if found_overlapping_addresses:
+    if found_an_overlapping_addresses:
         print("\nOverlapping Address found!")
     else:
         print("\nNo overlapping Address found!")
 
-    return found_overlapping_addresses
+    return found_an_overlapping_addresses
 
 
 def check_reserved_ram_area() -> bool:
     print("\nChecking reserved address space (for 'Auto Upload')")
-    found_reserved_ram_area_conflict = False
+    found_a_reserved_ram_area_conflict = False
     for key, value in data_area_dict.items():
         if key < RESERVED_MEMORY_ADDRESS:
-            found_reserved_ram_area_conflict = True
+            found_a_reserved_ram_area_conflict = True
             print(f'{hex(key)} {value} conflicts with reserved RAM address space')
 
-    return found_reserved_ram_area_conflict
-
-display = Display()
-
-json_ok = deserialize_json_check(display)
-
-data_area_dict = generate_data_area_dict_and_print_masks_and_ctrl(display)
-
-found_overlapping_addresses = print_overlapping_memory_areas(data_area_dict)
-
-found_reserved_ram_area_conflict = check_reserved_ram_area()
+    return found_a_reserved_ram_area_conflict
 
 
-if found_overlapping_addresses is False and found_reserved_ram_area_conflict and json_ok:
+if __name__ == "__main__":
+
+    display = Display(None)
+
+    json_ok = deserialize_json_check(display)
+
+    data_area_dict = generate_data_area_dict_and_print_masks_and_ctrl(display)
+
+    FOUND_OVERLAPPING_ADDRESS = print_overlapping_memory_areas(data_area_dict)
+
+    FOUND_RESERVED_RAM_AREA_CONFLICT = check_reserved_ram_area()
+
+
+if FOUND_OVERLAPPING_ADDRESS is False and FOUND_RESERVED_RAM_AREA_CONFLICT and json_ok:
     print('\nSanity check passed!')
 else:
     print("\nSanity check FAILED!")
-
